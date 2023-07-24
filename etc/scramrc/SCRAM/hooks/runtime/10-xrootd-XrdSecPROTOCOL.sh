89c5e262026a8623f033fc78008d2fe9decd3d3f
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-07-24 14:46:45 +0200

    Added scram runtime site hooks for xrootd-XrdSecPROTOCOL

---
+++ etc/scramrc/SCRAM/hooks/runtime/10-xrootd-XrdSecPROTOCOL.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+[ -z "${XrdSecPROTOCOL}" ] || exit 0
+[ ! -z "${JOB_GLIDEIN_Factory}" ] || exit 0
+[ ! -z "${SCRAM}" ] || SCRAM="scram"
+ver=$(${SCRAM} tool info xrootd 2>&1 | grep '^Version *: ' | sed 's|^Version *: *||;s| *$||' | cut -d. -f1,2)
+if [[ "${ver}" =~ ^(4.[6-9]|5.0)$ ]] ; then
+  klist -s >/dev/null 2>&1 || echo "RUNTIME:variable:XrdSecPROTOCOL=gsi,ztn,unix"
+fi
