6a7f24dd7d368190721f0b26a9be9d48df0d6641
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-08-23 11:18:12 +0200

    Update scr4am runtime hook for XrdSecPROTOCOL

--- etc/scramrc/SCRAM/hooks/runtime/10-xrootd-XrdSecPROTOCOL.sh
+++ etc/scramrc/SCRAM/hooks/runtime/10-xrootd-XrdSecPROTOCOL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 [ -z "${XrdSecPROTOCOL}" ] || exit 0
-[ ! -z "${JOB_GLIDEIN_Factory}" ] || exit 0
+[ ! -z "${GLIDEIN_Factory}" ] || exit 0
 [ ! -z "${SCRAM}" ] || SCRAM="scram"
 ver=$(${SCRAM} tool info xrootd 2>&1 | grep '^Version *: ' | sed 's|^Version *: *||;s| *$||' | cut -d. -f1,2)
 if [[ "${ver}" =~ ^(4.[6-9]|5.0)$ ]] ; then
