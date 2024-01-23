5b9ca2534cba4327f4ba3c17b853da15a0dfbf1e
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-01-23 18:01:10 +0100

    set GSL_CBLAS_LIB if gsl-config has ref to gslcblas

--- etc/scramrc/SCRAM/hooks/runtime/99-gsl-config.sh
+++ etc/scramrc/SCRAM/hooks/runtime/99-gsl-config.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 GSL_tool="${LOCALTOP}/config/toolbox/${SCRAM_ARCH}/tools/selected/gsl.xml"
 [ -e "${GSL_tool}" ] || exit 0
-! grep -q 'name="PATH"' "${GSL_tool}" || exit 0
 [ -z "${SCRAM}" ] && SCRAM=scram
 GSL_BASE=$(${SCRAM} tool tag gsl GSL_BASE)
 [ -e "${GSL_BASE}/bin/gsl-config" ] || exit 0
 if [ $(grep gslcblas ${GSL_BASE}/bin/gsl-config | wc -l) -gt 0 ] ; then
-  echo "RUNTIME:variable:GSL_CBLAS_LIB=-L$(${SCRAM} tool tag openblas LIBDIR) -l$(${SCRAM} tool tag openblas LIB)"
+  OpenBLAS_tool="${LOCALTOP}/config/toolbox/${SCRAM_ARCH}/tools/selected/openblas.xml"
+  if [ -e "${OpenBLAS_tool}" ] ; then
+    echo "RUNTIME:variable:GSL_CBLAS_LIB=-L$(${SCRAM} tool tag openblas LIBDIR) -l$(${SCRAM} tool tag openblas LIB)"
+  fi
 fi
+! grep -q 'name="PATH"' "${GSL_tool}" || exit 0
 echo "RUNTIME:path:append:PATH=${GSL_BASE}/bin"
